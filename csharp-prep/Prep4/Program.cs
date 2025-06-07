b7693e6032a8fcc5ecde1c64841f11f290c07ac1
dannewbybyui1 <danielenewbs@gmail.com>
2025-06-07 17:35:42 -0600

    update

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Student: Daniele Newby
+
+        Assignment assignment = new Assignment("Daniele Newby", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment("Daniele Newby", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment("Daniele Newby", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        Console.WriteLine();
     }
 }
